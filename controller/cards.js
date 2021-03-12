aaff27e9b76ea3c16a31dfb40a9dae86c35b3563
Moddoo <>
2021-03-12 17:24:59 -0600

    handleErrors

--- controller/cards.js
+++ controller/cards.js
@@ -1,40 +1,45 @@
 const cards = require('../models/card');
 
+const handleError = (res, status = 500, err = 'Internal Server Error') => res.status(status).send({ error: err });
+
 const getCards = (req, res) => {
   cards.find({})
     .populate('owner')
     .then((all) => res.send(all))
-    .catch(() => res.status(500).send({ message: 'Internal Server Error' }));
+    .catch(() => handleError(res));
 };
 
 const createCard = (req, res) => {
   cards.create({ ...req.body, owner: req.user._id })
     .then((card) => res.send(card))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'ValidationError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const deleteCard = (req, res) => {
   cards.findByIdAndDelete(req.params.cardId)
-    .then((card) => (card ? res.send({ message: 'Card Deleted' }) : res.status(404).send({ message: 'Not Found' })))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .orFail(() => handleError(res, 404, 'Not Found'))
+    .then(() => res.send({ message: 'Card Deleted' }))
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const likeCard = (req, res) => {
   cards.findByIdAndUpdate(req.params.cardId, {
     $addToSet: { likes: req.user._id },
   }, { new: true })
+    .orFail(() => handleError(res, 404, 'Not Found'))
     .populate('owner')
     .then((card) => res.send(card))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const dislikeCard = (req, res) => {
   cards.findByIdAndUpdate(req.params.cardId, {
     $pull: { likes: req.user._id },
   }, { new: true })
+    .orFail(() => handleError(res, 404, 'Not Found'))
     .populate('owner')
     .then((card) => res.send(card))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 module.exports = {
--- controller/users.js
+++ controller/users.js
@@ -1,35 +1,40 @@
 const users = require('../models/user');
 
+const handleError = (res, status = 500, err = 'Internal Server Error') => res.status(status).send({ error: err });
+
 const getUsers = (req, res) => {
   users.find({})
     .then((all) => res.send(all))
-    .catch(() => res.status(500).send({ message: 'Internal Server Error' }));
+    .catch(() => handleError(res));
 };
 
 const getUser = (req, res) => {
   users.findById(req.params.userId)
+    .orFail(() => handleError(res, 404, 'Not Found'))
     .then((user) => res.send(user))
-    .catch((err) => res.status(404).send({ error: err.message }));
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const createUser = (req, res) => {
   users.create(req.body)
     .then((user) => res.send(user))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'ValidationError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
   users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .orFail(() => handleError(res, 404, 'Not Found'))
     .then((user) => res.send(user))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   users.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .orFail(() => handleError(res, 404, 'Not Found'))
     .then((user) => res.send(user))
-    .catch((err) => res.status(400).send({ error: err.message }));
+    .catch((err) => (err.name === 'CastError' ? handleError(res, 400, err.message) : handleError(res)));
 };
 
 module.exports = {
